f6d1fba3976dd344dfd2c647111958e81be462ce
ebnail <51423625+ebnail@users.noreply.github.com>
2022-12-14 20:28:08 -0600

    Create MortgageCalculator.java

---
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -0,0 +1,64 @@
+package com.h2;
+
+import java.text.DecimalFormat;
+
+public class MortgageCalculator {
+	
+	
+	private long loanAmount;
+	private int termInYears;
+	private float annualRate;
+	private double monthlyPayment;
+	
+	public MortgageCalculator(long loanAmount, int termInYears,float annualRate) {
+		this.loanAmount = loanAmount;
+		this.termInYears = termInYears;
+		this.annualRate = annualRate;
+		
+	}
+	
+	private int getNumberOfPayments() {
+		return this.termInYears*12;
+	}
+	
+	private float getMonthlyInterestRate() {
+		float interestRate =this.annualRate/100;  
+		return interestRate/12;
+	}
+
+	public void calculateMonthlyPayment() {
+		
+		float r = this.getMonthlyInterestRate();
+	    float ratePower = (float) Math.pow((1 + this.getMonthlyInterestRate()), this.getNumberOfPayments());
+	    this.monthlyPayment = this.loanAmount*(r*ratePower)/(ratePower-1);
+	    
+//		M = P(r(1+r)^n/(((1+r)^n)-1)
+//
+//				M is the calculated monthly mortgage payment,  
+//
+//				P is the principal amount, represented by loanAmount in our class,  
+//
+//				r is the monthly interest rate, which you can find by calling getMonthlyInterestRate().  
+//
+//				n is the total number of payments which you can find by calling getNumberOfPayments().
+	}
+	
+	public String toString() {
+		DecimalFormat df = new DecimalFormat("####0.00");
+		return "monthlyPayment: " + df.format(this.monthlyPayment);
+	}
+	
+	public static void main(String[] args) {
+		if(args.length!=3)
+			System.out.print("Bad Data\n");
+		else {
+			long loanAmount = Long.parseLong(args[0]);
+			int termInYears = Integer.parseInt(args[1]); 
+			float annualRate = Float.parseFloat(args[2]);
+			MortgageCalculator calc = new MortgageCalculator(loanAmount,termInYears,annualRate);
+			calc.calculateMonthlyPayment();
+			System.out.print(calc.toString()+"\n");
+		}
+	}
+
+}
